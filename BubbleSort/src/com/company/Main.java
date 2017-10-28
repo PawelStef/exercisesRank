e463bad69000f3201fc79917edd532fff31fa9a7
Stef\Paweł <paw.stefanowski@gmail.com>
2017-10-28 23:14:37 +0200

    no message

--- BubbleSort/src/com/company/Main.java
+++ BubbleSort/src/com/company/Main.java
@@ -10,9 +10,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of numbers:");
         int n = in.nextInt();
         int[] a = new int[n];
         for(int a_i=0; a_i < n; a_i++){
+            System.out.println("Give a number");
             a[a_i] = in.nextInt();
         }
         Solution sol = new Solution();
